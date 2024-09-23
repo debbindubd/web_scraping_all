1e7c2c682a8b515c9958f65b98025d285e61e9d4
debbindubd <debbindubd@gmail.com>
2024-09-23 12:44:39 +0600

    Adding the Practice part

---
+++ practice/part4/bookscraper/bookscraper/spiders/bookspider.py
@@ -0,0 +1,49 @@
+import scrapy
+
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com"]
+
+    def parse(self, response):
+        books =  response.css('article.product_pod')
+
+        for book in books:
+            relative_url = book.css('h3 a ::attr(href)').get()
+
+            if 'catalogue/' in relative_url:
+                book_url = 'https://books.toscrape.com/' + relative_url
+            else:
+                book_url = 'https://books.toscrape.com/catalogue/' + relative_url
+            yield response.follow(book_url, callback = self.parse_book_page)
+
+
+        next_page = response.css('li.next a ::attr(href)').get()
+        if next_page is not None:
+            if 'catalogue/' in next_page:
+                next_page_url = 'https://books.toscrape.com/' + next_page
+            else:
+                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+            yield response.follow(next_page_url, callback=self.parse)
+        
+        
+    def parse_book_page(self, response):
+
+        table_rows = table_rows = response.css('table tr')
+
+        yield{
+            'url' : response.url,
+            'catagory': response.xpath('//*[@id="default"]/div[1]/div/ul/li[3]/a/text()').get(),
+            'title' : response.css('.product_main h1::text').get(),
+            'product_type': table_rows[1].css('td ::text').get(),
+            'price_excl_tax': table_rows[2].css('td ::text').get(),
+            'price_incl_tax': table_rows[3].css('td ::text').get(),
+            'tax': table_rows[4].css('td ::text').get(),
+            'star': response.css('p.star-rating').attrib['class'],
+            'description': response.xpath('//*[@id="content_inner"]/article/p/text()').get(),
+            'pirce' : response.css('p.price_color ::text').get()
+        }
+            
+
+            
